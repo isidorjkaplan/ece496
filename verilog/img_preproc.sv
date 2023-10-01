6be87ad3c99220f2b987db87be6d9d7d089121ff
Isidor Kaplan <isidor.kaplan@utoronto.ca>
2023-10-01 12:11:55 -0400

    Adding in more debug signals that we can tap to try and understand what is going on

--- verilog/img_preproc.sv
+++ verilog/img_preproc.sv
@@ -85,8 +85,8 @@ module img_preproc_top(
         .outport_pixel_b_o(outport_pixel_b_o),
         .idle_o(idle_o),
         
-        .debug_signals(debug_signals[31:12]),
-        .debug_conditions(debug_conditions[23:5])
+        .debug_signals(debug_signals[20:0]),
+        .debug_conditions(debug_conditions[18:0])
         );
 
 
@@ -101,25 +101,25 @@ module img_preproc_top(
 
     // DEBUG TAPS
 
-    assign debug_signals[1] = byte_count;
-    assign debug_signals[2] = inport_strb_i;
-    assign debug_signals[3] = outport_height_o;
-    assign debug_signals[4] = outport_width_o;
-    assign debug_signals[5] = outport_pixel_x_o;
-    assign debug_signals[6] = outport_pixel_y_o;
-    assign debug_signals[7] = outport_pixel_r_o;
-    assign debug_signals[8] = outport_pixel_g_o;
-    assign debug_signals[9] = outport_pixel_b_o;
-    assign debug_signals[10] = {idle_o, 3'b0, outport_valid_o, 3'b0, inport_accept_o};
-    assign debug_signals[11] = {upstream_stall, 3'b0, downstream_stall};
+    assign debug_signals[31] = byte_count;
+    assign debug_signals[30] = inport_strb_i;
+    assign debug_signals[29] = outport_height_o;
+    assign debug_signals[28] = outport_width_o;
+    assign debug_signals[27] = outport_pixel_x_o;
+    assign debug_signals[26] = outport_pixel_y_o;
+    assign debug_signals[25] = outport_pixel_r_o;
+    assign debug_signals[24] = outport_pixel_g_o;
+    assign debug_signals[23] = outport_pixel_b_o;
+    assign debug_signals[22] = {idle_o, 3'b0, outport_valid_o, 3'b0, inport_accept_o};
+    assign debug_signals[21] = {upstream_stall, 3'b0, downstream_stall};
 
 
     // DEBUG CONDITIONS
-    assign debug_conditions[0] = outport_valid_o;
-    assign debug_conditions[1] = inport_accept_o;
-    assign debug_conditions[2] = idle_o;
-    assign debug_conditions[3] = downstream_stall;
-    assign debug_conditions[4] = upstream_stall;
+    assign debug_conditions[23] = outport_valid_o;
+    assign debug_conditions[22] = inport_accept_o;
+    assign debug_conditions[21] = idle_o;
+    assign debug_conditions[20] = downstream_stall;
+    assign debug_conditions[19] = upstream_stall;
 
     
 
