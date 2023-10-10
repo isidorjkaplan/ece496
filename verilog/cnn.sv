3d48e0aa1732a1794385564e2c0865c74a8cda5d
Albert Huang <albert475000@gmail.com>
2023-10-10 00:00:20 -0400

    try to add multiple layers like what the software CNN is like
    software CNN could be found in pytorch/mnist.ipynb

--- verilog/cnn.sv
+++ verilog/cnn.sv
@@ -25,7 +25,7 @@ module cnn_top #(parameter
     parameter LAYER0_KERNAL_SIZE=3;
     parameter LAYER0_WIDTH=28;
     parameter LAYER0_IN_CHANNELS=1;
-    parameter LAYER0_OUT_CHANNELS=1;
+    parameter LAYER0_OUT_CHANNELS=4;
     parameter LAYER0_NUM_KERNALS=1;
 
     logic  [ WEIGHT_BITS-1 : 0 ] layer0_kernal_weights_i[LAYER0_OUT_CHANNELS][LAYER0_IN_CHANNELS][LAYER0_KERNAL_SIZE][LAYER0_KERNAL_SIZE];
@@ -117,20 +117,104 @@ module cnn_top #(parameter
     assign layer0_out_row_accept_i = pool0_in_row_accept_o;
     assign pool0_in_row_last_i = layer0_out_row_last_o;
 
-    // POOL0 -> OUT glue logic
+    // CNN LAYER 1
+
+    parameter LAYER1_KERNAL_SIZE=3;
+    parameter LAYER1_WIDTH=POOL0_OUT_WIDTH;
+    parameter LAYER1_IN_CHANNELS=POOL0_CHANNELS;
+    parameter LAYER1_OUT_CHANNELS=10;
+    parameter LAYER1_NUM_KERNALS=1;
+
+    logic  [ WEIGHT_BITS-1 : 0 ] layer1_kernal_weights_i[LAYER1_OUT_CHANNELS][LAYER1_IN_CHANNELS][LAYER1_KERNAL_SIZE][LAYER1_KERNAL_SIZE];
+
+    logic [VALUE_BITS-1 : 0] layer1_in_row_i[LAYER1_WIDTH][LAYER1_IN_CHANNELS];
+    logic layer1_in_row_valid_i, layer1_in_row_accept_o, layer1_in_row_last_i;
+    
+    logic [VALUE_BITS -1 : 0] layer1_out_row_o[LAYER1_WIDTH][LAYER1_OUT_CHANNELS];
+    logic layer1_out_row_valid_o;
+    logic layer1_out_row_accept_i;
+    logic layer1_out_row_last_o;
+    
+    cnn_layer #(
+        .KERNAL_SIZE(LAYER1_KERNAL_SIZE), .NUM_KERNALS(LAYER1_NUM_KERNALS), 
+        .WIDTH(LAYER1_WIDTH), .VALUE_BITS(VALUE_BITS), .WEIGHT_BITS(WEIGHT_BITS), .WEIGHT_Q_SHIFT(WEIGHT_Q_SHIFT), 
+        .IN_CHANNELS(LAYER1_IN_CHANNELS), .OUT_CHANNELS(LAYER1_OUT_CHANNELS)
+    ) layer1(
+        // General
+        .clock_i(clock), .reset_i(reset),
+        .kernal_weights_i(layer1_kernal_weights_i),
+        // INPUT INFO
+        .in_row_i(layer1_in_row_i),
+        .in_row_valid_i(layer1_in_row_valid_i),
+        .in_row_accept_o(layer1_in_row_accept_o),
+        .in_row_last_i(layer1_in_row_last_i),
+        // OUT INFO
+        .out_row_o(layer1_out_row_o),
+        .out_row_valid_o(layer1_out_row_valid_o),
+        .out_row_accept_i(layer1_out_row_accept_i),
+        .out_row_last_o(layer1_out_row_last_o)
+    );
+
+    // POOL0 -> LAYER1
+    assign layer1_in_row_i = pool0_out_row_o;
+    assign layer1_in_row_valid_i = pool0_out_row_valid_o;
+    assign pool0_out_row_accept_i = layer1_in_row_accept_o;
+    assign layer1_in_row_last_i = pool0_out_row_last_o;
+
+    // POOLING LAYER 1
+
+    parameter POOL1_KERNAL_SIZE=2;
+    parameter POOL1_NUM_KERNALS=1;
+    parameter POOL1_WIDTH=LAYER1_WIDTH;
+    parameter POOL1_CHANNELS=LAYER1_OUT_CHANNELS;
+    parameter POOL1_OUT_WIDTH = POOL1_WIDTH / POOL1_KERNAL_SIZE;
+
+    logic [VALUE_BITS - 1 : 0] pool1_in_row_i[POOL1_WIDTH][POOL1_CHANNELS];
+    logic pool1_in_row_valid_i, pool1_in_row_accept_o, pool1_in_row_last_i;
+    
+    logic [VALUE_BITS - 1 : 0] pool1_out_row_o[POOL1_OUT_WIDTH][POOL1_CHANNELS];
+    logic pool1_out_row_valid_o;
+    logic pool1_out_row_accept_i;
+    logic pool1_out_row_last_o;
+
+    max_pooling_layer #(
+        .KERNAL_SIZE(POOL1_KERNAL_SIZE), .NUM_KERNALS(POOL1_NUM_KERNALS), 
+        .WIDTH(POOL1_WIDTH), .VALUE_BITS(VALUE_BITS), .CHANNELS(POOL1_CHANNELS)
+    ) pool1 (
+        // General
+        .clock_i(clock), .reset_i(reset),
+        // INPUT INFO
+        .in_row_i(pool1_in_row_i),
+        .in_row_valid_i(pool1_in_row_valid_i),
+        .in_row_accept_o(pool1_in_row_accept_o),
+        .in_row_last_i(pool1_in_row_last_i),
+        // OUT INFO
+        .out_row_o(pool1_out_row_o),
+        .out_row_valid_o(pool1_out_row_valid_o),
+        .out_row_accept_i(pool1_out_row_accept_i),
+        .out_row_last_o(pool1_out_row_last_o)
+    );
+
+    // LAYER1 -> POOL1 GLUE LOGIC
+    assign pool1_in_row_i = layer1_out_row_o;
+    assign pool1_in_row_valid_i = layer1_out_row_valid_o;
+    assign layer1_out_row_accept_i = pool1_in_row_accept_o;
+    assign pool1_in_row_last_i = layer1_out_row_last_o;
+
+    // POOL1 -> OUT glue logic
 
 
-    logic [VALUE_BITS-1 : 0] out_row_par[POOL0_OUT_WIDTH*POOL0_CHANNELS];
-    serialize #(.N(POOL0_OUT_WIDTH*POOL0_CHANNELS), .DATA_BITS(VALUE_BITS), .DATA_PER_WORD(VALUES_PER_WORD)) ser2par(
+    logic [VALUE_BITS-1 : 0] out_row_par[POOL1_OUT_WIDTH*POOL1_CHANNELS];
+    serialize #(.N(POOL1_OUT_WIDTH*POOL1_CHANNELS), .DATA_BITS(VALUE_BITS), .DATA_PER_WORD(VALUES_PER_WORD)) ser2par(
         .clock(clock), .reset(reset), 
-        .in_data(out_row_par), .in_valid(pool0_out_row_valid_o),
+        .in_data(out_row_par), .in_valid(pool1_out_row_valid_o),
         .out_data(out_data), .out_valid(out_valid),
-        .downstream_stall(downstream_stall), .upstream_stall(pool0_out_row_accept_i)
+        .downstream_stall(downstream_stall), .upstream_stall(pool1_out_row_accept_i)
     );
     always_comb begin
-        for (int x = 0; x < POOL0_OUT_WIDTH; x++) begin
-            for (int out_ch = 0; out_ch < POOL0_CHANNELS; out_ch++) begin
-                out_row_par[x + out_ch*POOL0_OUT_WIDTH] = pool0_out_row_o[x][out_ch];
+        for (int x = 0; x < POOL1_OUT_WIDTH; x++) begin
+            for (int out_ch = 0; out_ch < POOL1_CHANNELS; out_ch++) begin
+                out_row_par[x + out_ch*POOL1_OUT_WIDTH] = pool1_out_row_o[x][out_ch];
             end
         end
     end
@@ -146,6 +230,18 @@ module cnn_top #(parameter
                         layer0_kernal_weights_i[out_ch][in_ch][x][y] 
                         = (1<<WEIGHT_Q_SHIFT)
                             / (LAYER0_OUT_CHANNELS*LAYER0_IN_CHANNELS*LAYER0_KERNAL_SIZE*LAYER0_KERNAL_SIZE);
+                    end
+                end
+            end
+        end
+        // LAYER 1 WEIGHTS
+        for (int out_ch = 0; out_ch < LAYER1_OUT_CHANNELS; out_ch++) begin
+            for (int in_ch = 0; in_ch < LAYER1_IN_CHANNELS; in_ch++) begin
+                for (int x = 0; x < LAYER1_KERNAL_SIZE; x++) begin
+                    for (int y = 0; y < LAYER1_KERNAL_SIZE; y++) begin
+                        layer1_kernal_weights_i[out_ch][in_ch][x][y] 
+                        = (1<<WEIGHT_Q_SHIFT)
+                            / (LAYER1_OUT_CHANNELS*LAYER1_IN_CHANNELS*LAYER1_KERNAL_SIZE*LAYER1_KERNAL_SIZE);
                     end
                 end
             end
