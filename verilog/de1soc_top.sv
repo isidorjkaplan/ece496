f5fe2cc16a1d63fe4746613cbf9732d5a9e82b80
Isidor Kaplan <isidor.kaplan@utoronto.ca>
2023-10-10 09:22:00 -0400

    Merging in alberts changes and moving model definition to its own file so that the CNN library and the specific MNIST model are in seperate files

--- verilog/de1soc_top.sv
+++ verilog/de1soc_top.sv
@@ -113,3 +113,34 @@ module serialize #(parameter N, DATA_BITS, DATA_PER_WORD, WORD_SIZE=32) (
     //assign out_data = data_buffer[data_idx];
 
 endmodule
+
+
+// This just takes a value and registers it
+module axi_buffer(
+    input wire clock, 
+    input wire reset, //+ve synchronous reset
+
+    input wire [ 31 : 0 ] in_data,
+    input wire in_valid,
+
+    output reg [ 31 : 0 ] out_data,
+    output reg out_valid, 
+
+    input wire downstream_stall,
+    output wire upstream_stall
+);
+
+    assign upstream_stall = out_valid && downstream_stall;
+    
+    always_ff@(posedge clock) begin
+        if (reset) begin
+            out_data <= 0;
+            out_valid <= 0;
+        end
+        else if (!downstream_stall || !out_valid) begin
+            out_data <= in_data;
+            out_valid <= in_valid;
+        end  
+    end
+endmodule 
+ 
