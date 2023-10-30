607cb7317bfd080bfbb119093354b6606d699066
Albert Huang <albert475000@gmail.com>
2023-10-29 21:35:01 -0400

    maxpool working except for last

---
+++ sim/maxpool_tb.sv
@@ -0,0 +1,306 @@
+module tb();
+    // Define some local parameters useful for simulation
+    localparam VALUE_BITS = 24;
+    localparam N = 12;
+    localparam CLK_PERIOD = 20;                    // Clock period is 20ns
+    localparam QSTEP = CLK_PERIOD/4;                // Time step of a quarter of a clock period
+    localparam TIMESTEP = CLK_PERIOD/10;        // Time step of one tenth of a clock period
+    localparam POOL_SIZE = 2;                    // Convolution filter size (3x3)
+    localparam CHANNELS = 4;
+    // You have to set the project directory for the testbench to operate correctly
+    localparam PROJECT_DIR = "";
+    // localparam TEST_IMAGE = {PROJECT_DIR, "mnist/file_0_5.pgm"};
+    localparam TEST_IMAGE_0 = {PROJECT_DIR, "mnist/test.pgm"};
+    localparam TEST_IMAGE_1 = {PROJECT_DIR, "mnist/test1.pgm"};
+
+    // Declare signals for the DUT interface
+    logic clk;
+    logic reset;
+    logic i_valid;
+    logic i_ready;
+    logic i_last;
+    logic o_valid;
+    logic o_ready;
+    logic o_last;
+    logic signed [VALUE_BITS-1:0]  i_data;
+    logic signed [VALUE_BITS-1:0]  o_data;
+
+    logic signed [VALUE_BITS-1:0]  i_data_int;
+    logic signed [VALUE_BITS-1:0]  o_data_int;
+
+    assign i_data_int = i_data >> N;
+    assign o_data_int = o_data >> N;
+
+    // Generate a 50MHz clock
+    initial clk = 1'b1;
+    always #(CLK_PERIOD/2) clk = ~clk;
+
+    // Instantiation of the DUT circuit
+    max_pooling_layer_single_channel #(
+        .WIDTH(28), 
+        .POOL_SIZE(POOL_SIZE), 
+        .VALUE_BITS(VALUE_BITS)
+    ) dut (
+        .clk(clk),
+        .reset(reset),
+        .i_data(i_data),
+        .i_valid(i_valid),
+        .i_ready(i_ready),
+        .i_last(i_last),
+        .o_data(o_data),
+        .o_valid(o_valid),
+        .o_ready(o_ready),
+        .o_last(o_last)
+    );
+
+    // Declare more signals used for testing
+    integer test_image, result_image;
+    integer image_width, image_height;
+    integer temp, pixel_id, pixel_val;
+    logic [8000:0] line;
+    logic valid_stall_tested = 0;
+    logic [31:0] saved_i_x;
+    integer out_id;
+    integer out_channel;
+    integer current_time = 0;
+    logic ready_stall_tested = 0;
+    integer repeatCount;
+
+    // Producer Process
+    initial begin
+        // test image 1
+        test_image = $fopen(TEST_IMAGE_0, "r");
+
+        // Read the image PGM header
+        temp = $fgets(line, test_image);
+        temp = $fgets(line, test_image);
+        temp = $fscanf(test_image, "%d %d\n", image_width, image_height);
+        $display("Image width = %d, Image height = %d\n", image_width, image_height);
+        temp = $fgets(line, test_image);
+
+        // i_valid starts low
+        i_valid = 1'b0;
+        i_data = 0;
+        i_last = 0;
+
+        // Hold the reset high for 3.5 cycles
+        // Doing this to ensure the design is fully reset if someone uses a multi-cycle 
+        // reset strategy, and that the reset goes low away from any clock edges to 
+        // avoid any subtle timing issues when doing gate-level (timing) simulation.
+        reset = 1'b1;
+        #(3*CLK_PERIOD);
+        #(2*QSTEP);
+        reset = 1'b0;
+        #(2*QSTEP);
+
+        // Stream in the values of the image pixels
+        for(pixel_id = 0; pixel_id < (image_width*image_height); pixel_id = pixel_id + 1) begin		
+            // Start at positive edge. Advance quarter cycle
+            #(QSTEP);
+            
+            // Prepare an input value (if it is first or last pixel of the row, insert zero for padding in case of a 3x3 filte. Otherwise, insert pixel)
+            temp = $fscanf(test_image, "%d ", pixel_val); 
+
+            i_data = pixel_val << N;
+            saved_i_x = i_data;
+            if(pixel_id == (image_width*image_height)-1) begin
+                i_last = 1;
+            end
+            
+            // Advance half a cycle
+            #(2*QSTEP);
+            
+            // Check i_ready and set i_valid
+            i_valid = 1'b1;
+            while(!i_ready) begin
+                // If DUT claims to not be ready, then it shouldn't be an issue if we 
+                // give it an erroneous value.
+                i_data = 128;
+                i_valid = 1'b0;
+                
+                // Wait for a clock period before checking i_ready again
+                #(CLK_PERIOD);
+                
+                // Restore the correct value of i_x if we're going to go out of this while loop
+                i_data = saved_i_x;
+                i_valid = 1'b1;
+            end
+            
+            // Test that DUT stalls properly if we don't give it a valid input
+            if (pixel_id == image_width/4 && !valid_stall_tested) begin
+                i_valid = 1'b0;
+                i_data = 23; // Strange input to make sure it is ignored.
+                #(3*CLK_PERIOD);
+                
+                // DUT may not be ready at this point, so wait until it is
+                while(!i_ready) begin
+                    #(CLK_PERIOD);
+                end
+
+                i_data = saved_i_x;
+                i_valid = 1'b1;
+                valid_stall_tested = 1;
+            end
+            
+            // Advance another quarter cycle to next positive edge
+            #(QSTEP);
+        end
+        // Advance another quarter cycle to next positive edge
+        #(QSTEP);
+        i_last = 0;
+        i_valid = 0;
+        $fclose(test_image);
+        // Advance another 3 quarter cycles to next positive edge
+        #(3*QSTEP);
+
+        // test image 2
+        test_image = $fopen(TEST_IMAGE_1, "r");
+
+        // Read the image PGM header
+        temp = $fgets(line, test_image);
+        temp = $fgets(line, test_image);
+        temp = $fscanf(test_image, "%d %d\n", image_width, image_height);
+        $display("Image width = %d, Image height = %d\n", image_width, image_height);
+        temp = $fgets(line, test_image);
+
+        // i_valid starts low
+        i_valid = 1'b0;
+        i_data = 0;
+        i_last = 0;
+
+        // // Hold the reset high for 3.5 cycles
+        // // Doing this to ensure the design is fully reset if someone uses a multi-cycle 
+        // // reset strategy, and that the reset goes low away from any clock edges to 
+        // // avoid any subtle timing issues when doing gate-level (timing) simulation.
+        // reset = 1'b1;
+        // #(3*CLK_PERIOD);
+        // #(2*QSTEP);
+        // reset = 1'b0;
+        // #(2*QSTEP);
+
+        // Stream in the values of the image pixels
+        for(pixel_id = 0; pixel_id < (image_width*image_height); pixel_id = pixel_id + 1) begin		
+            // Start at positive edge. Advance quarter cycle
+            #(QSTEP);
+            
+            // Prepare an input value (if it is first or last pixel of the row, insert zero for padding in case of a 3x3 filte. Otherwise, insert pixel)
+            temp = $fscanf(test_image, "%d ", pixel_val); 
+
+            i_data = pixel_val << N;
+            saved_i_x = i_data;
+            if(pixel_id == (image_width*image_height)-1) begin
+                i_last = 1;
+            end
+            
+            // Advance half a cycle
+            #(2*QSTEP);
+            
+            // Check i_ready and set i_valid
+            i_valid = 1'b1;
+            while(!i_ready) begin
+                // If DUT claims to not be ready, then it shouldn't be an issue if we 
+                // give it an erroneous value.
+                // i_data[0] = 128;
+                // i_valid = 1'b0;
+                
+                // Wait for a clock period before checking i_ready again
+                #(CLK_PERIOD);
+                
+                // Restore the correct value of i_x if we're going to go out of this while loop
+                i_data = saved_i_x;
+                i_valid = 1'b1;
+            end
+            
+            // Test that DUT stalls properly if we don't give it a valid input
+            if (pixel_id == image_width/4 && !valid_stall_tested) begin
+                i_valid = 1'b0;
+                i_data = 23; // Strange input to make sure it is ignored.
+                #(3*CLK_PERIOD);
+                
+                // DUT may not be ready at this point, so wait until it is
+                while(!i_ready) begin
+                    #(CLK_PERIOD);
+                end
+
+                i_data = saved_i_x;
+                i_valid = 1'b1;
+                valid_stall_tested = 1;
+            end
+            
+            // Advance another quarter cycle to next positive edge
+            #(QSTEP);
+        end
+        // Advance another quarter cycle to next positive edge
+        #(QSTEP);
+        i_last = 0;
+        i_valid = 0;
+        $fclose(test_image);
+    end
+
+    // Consumer process
+    initial begin
+        // Downstream consumer device is initially ready to receive data
+	    o_ready = 1'b1;
+
+        // Delay until just before the first posedge
+        #(CLK_PERIOD - TIMESTEP);
+        current_time = current_time + (CLK_PERIOD - TIMESTEP);
+
+        for(repeatCount = 0; repeatCount < 2; repeatCount = repeatCount+1)begin
+            
+            // Open files to write the result image, difference image and simulation log
+            result_image = $fopen({PROJECT_DIR, $sformatf("mnist/pool_out_%0d.pgm", repeatCount)}, "w");
+            // Write result image PGM header
+            $fwrite(result_image, "P2\n");
+            $fwrite(result_image, "# Generated by ECE496 Verilog testbench\n");
+            $fwrite(result_image, "%d %d\n", image_width/POOL_SIZE, image_height/POOL_SIZE);
+            $fwrite(result_image, "255\n");
+
+            for (out_id = 0; out_id < (image_width/POOL_SIZE) * (image_height/POOL_SIZE); out_id = out_id) begin
+                // We are now at the point just before the posedge. Check o_valid and compare results.
+                if (o_valid) begin                    
+                    if(out_id % (image_width/POOL_SIZE) == 0 && out_id!=0) begin
+                        $fwrite(result_image, "\n");
+                    end            
+                    // Write the output pixel and difference between output and golden result
+                    $fwrite(result_image, "%d ", o_data[N+:(VALUE_BITS-N)]);
+                    // Increment our loop counter
+                    out_id = out_id + 1;
+                end
+                
+                // Advance to positive edge
+                #(TIMESTEP);
+                current_time = current_time + (TIMESTEP);
+
+                // Then advance another quarter cycle
+                #(QSTEP);
+                current_time = current_time + (QSTEP);
+
+                // Set o_ready
+                o_ready = 1'b1;
+                
+                // Test that DUT stalls properly if receiver isn't ready
+                if (out_id == image_height/4 && !ready_stall_tested) begin
+                    o_ready = 1'b0;
+                    // Wait for 6 clock periods
+                    #(6*CLK_PERIOD);
+                    current_time = current_time + (6*CLK_PERIOD);
+                    // Then restore o_ready
+                    o_ready = 1'b1;
+                    ready_stall_tested = 1;
+                end
+                
+                // Then advance to just before the next posedge
+                #(3*QSTEP - TIMESTEP);
+                current_time = current_time + (3*QSTEP - TIMESTEP);
+            end
+
+            $fclose(result_image);
+            
+        end
+        #100;
+        $display("Current Timestep: %0t", $time);
+	    $stop(0);
+    end
+
+endmodule
--- verilog/maxpool2d.sv
+++ verilog/maxpool2d.sv
@@ -2,7 +2,7 @@ module max_pooling_layer #(
     parameter WIDTH = 28, 
     parameter POOL_SIZE = 2, 
     parameter VALUE_BITS = 32, 
-    parameter CHANNELS = 1,
+    parameter CHANNELS = 1
 ) (
     // general
     input   logic                               clk,                // Operating clock
@@ -34,8 +34,7 @@ module max_pooling_layer #(
             max_pooling_layer_single_channel #(
                 .WIDTH(WIDTH),
                 .POOL_SIZE(POOL_SIZE),
-                .VALUE_BITS(VALUE_BITS),
-                .CHANNELS(CHANNELS)
+                .VALUE_BITS(VALUE_BITS)
             ) maxpool (
                 .clk(clk),
                 .reset(reset),
@@ -56,8 +55,7 @@ endmodule
 module max_pooling_layer_single_channel #(
     parameter WIDTH = 28, 
     parameter POOL_SIZE = 2, 
-    parameter VALUE_BITS = 32, 
-    parameter CHANNELS = 1
+    parameter VALUE_BITS = 32
 ) (
     // general
     input   logic                               clk,        // Operating clock
@@ -75,91 +73,248 @@ module max_pooling_layer_single_channel #(
     input   logic                               o_ready,    // Set to 1 if this block is ready to receive a new pixel
     output  logic                               o_last      // Set to 1 if output pixel is last of image
 );
-    logic signed [VALUE_BITS - 1 : 0]   buffer_taps[POOL_SIZE][POOL_SIZE];
-    logic                               buffer_ready;
-    logic                               buffer_valid;
-    logic                               buffer_last;
+    localparam O_WIDTH = WIDTH/POOL_SIZE;
+    localparam I_WIDTH = O_WIDTH*POOL_SIZE;
 
-    // generate output data
-    logic signed [VALUE_BITS-1:0]       o_data_q;
-    logic signed [VALUE_BITS-1:0]       next_o_data;
-    logic                               o_last_q;
-    logic                               consumed;
+    // general logic
+    logic                           in_or_out_q;            // 0 for in, 1 for out
+    logic                           next_in_or_out;
+    logic                           i_ready_q;
+    logic                           next_i_ready;
+    logic                           got_last_q;
 
-    assign o_valid = ~consumed;
-    assign o_last = o_last_q;
-    assign o_data = o_data_q;
+    // ram logic
+    logic [$clog2(O_WIDTH)-1:0]     ram_w_addr_q; 
+    logic [$clog2(O_WIDTH)-1:0]     next_ram_w_addr; 
+    logic [$clog2(O_WIDTH)-1:0]     ram_r_addr;
+    logic [$clog2(O_WIDTH)-1:0]     prev_ram_r_addr_q;
+    logic signed [VALUE_BITS-1:0]   ram_r_data;
+    logic signed [VALUE_BITS-1:0]   ram_w_data;
+    logic                           write_en;
 
-    // if we don't have valid output then we can get new tap from buffer
-    // or if we do have valid output but it will be consumed next cycle
-    assign buffer_ready = consumed || (~consumed && o_ready);
+    // max pool logic
+    logic [$clog2(WIDTH):0]         in_counter_q;           // how many pixels we recieved for this row so far
+    logic [$clog2(WIDTH):0]         next_in_counter;
+    logic [$clog2(POOL_SIZE+1):0]   per_pool_counter_q;     // how many pixels we recieved for this pool window so far
+    logic [$clog2(POOL_SIZE+1):0]   next_per_pool_counter;
+    logic [$clog2(POOL_SIZE):0]     row_counter_q;          // which row we are working on rn
+    logic [$clog2(POOL_SIZE):0]     next_row_counter;
+    logic signed [VALUE_BITS-1:0]   prev_max_q;
+    logic signed [VALUE_BITS-1:0]   next_prev_max;
+    logic signed [VALUE_BITS-1:0]   new_data_q[POOL_SIZE];
+    logic [$clog2(POOL_SIZE):0]     new_data_idx_q;
+    logic [$clog2(POOL_SIZE):0]     next_new_data_idx;
 
-    // get max value
+    assign i_ready = i_ready_q;
+    
+
+    // main logic of interacting with ram
     always_comb begin
-        // set next_o_data to minimum value
-        next_o_data = '0;
-        next_o_data[VALUE_BITS-1] = 1'b1;
-        // get max value out of the tap values
-        // prob have a better way this feels inefficient
-        // for(int row = 0; row < POOL_SIZE; row++) begin
-        //     for(int col = 0; col < POOL_SIZE; col++) begin
-        //         if(next_o_data < buffer_taps[row][col]) begin
-        //             next_o_data = buffer_taps[row][col];
-        //         end
-        //     end
-        // end
-        // for testing
-        next_o_data = buffer_taps[0][0];
+        // default values
+        // general control
+        next_in_or_out = in_or_out_q;
+        next_i_ready = 0;
+        // ram control
+        next_ram_w_addr = ram_w_addr_q;
+        ram_r_addr = prev_ram_r_addr_q;
+        write_en = 0;
+        // max pool logic
+        next_in_counter = in_counter_q;
+        next_per_pool_counter = per_pool_counter_q;
+        next_row_counter = row_counter_q;
+        next_prev_max = prev_max_q;
+        next_new_data_idx = new_data_idx_q;
+        // output logic
+        o_data = '0;
+        o_valid = 0;
+
+        // if we are in input mode
+        if(~in_or_out_q) begin
+            // when processing useful pixels
+            if(in_counter_q < I_WIDTH) begin
+                case(per_pool_counter_q)
+                    // first cycle do nothing as we are waiting for
+                    // prev max on readdata
+                    // increment per_pool_counter
+                    // TODO: this cycle prob could be eliminated
+                    0: begin
+                        next_per_pool_counter = next_per_pool_counter + 1;
+                    end
+                    // second cycle assign prev max value and increment ram_r_addr
+                    1: begin
+                        // if we are on first row then prev_max shouldn't be read value
+                        if(row_counter_q == 0) begin
+                            next_prev_max = '0;
+                            next_prev_max[VALUE_BITS-1] = 1'b1;
+                        end
+                        else begin
+                            next_prev_max = ram_r_data;
+                        end
+                        // increment ram_r_addr
+                        ram_r_addr = ram_r_addr + 1;
+                        // next cycle we start reading in pixels, have i_ready high
+                        next_i_ready = 1;
+                        next_per_pool_counter = next_per_pool_counter + 1;
+                    end
+                    // last cycle for this pool window, reset relevant counters
+                    (POOL_SIZE + 2): begin
+                        // ram_w_data would have the correct values to be written in the ram now
+                        // increment ram_w_addr and have write_en high
+                        write_en = 1'b1;
+                        next_ram_w_addr = next_ram_w_addr + 1;
+                        // reset counters for next pool
+                        next_per_pool_counter = '0;
+                        next_new_data_idx = '0;
+                    end
+                    // default case is then trying to read in pixels
+                    default: begin
+                        // if data is valid then we would consume that data
+                        // increment relevant counters
+                        if(i_valid) begin
+                            next_in_counter = next_in_counter + 1;
+                            next_per_pool_counter = next_per_pool_counter + 1;
+                            next_new_data_idx = next_new_data_idx + 1;
+                        end
+                        // if we will not finish collecting the pixels for this pool window
+                        // keep i_ready high
+                        if(next_new_data_idx < POOL_SIZE) begin
+                            next_i_ready = 1;
+                        end
+                        // or keep i_ready high if this is last pixel of useful pixels
+                        // and we need to get rid of useless pixels
+                        if(next_in_counter == I_WIDTH && I_WIDTH != WIDTH) begin
+                            next_i_ready = 1;
+                        end
+                    end
+                endcase
+            end
+            // when we recived all pixels of a row
+            // i_ready should be low when in here
+            // if last is registered by here then this following output is the last output
+            // TODO: last implementation
+            else if(in_counter_q == WIDTH) begin
+                // if per_pool_counter_q not yet reset
+                if(per_pool_counter_q == POOL_SIZE + 2) begin
+                    // ram_w_data would have the correct values to be written in the ram now
+                    // have write_en high
+                    write_en = 1'b1;
+                end
+                // reset relevant signals 
+                next_row_counter = next_row_counter + 1;
+                ram_r_addr = '0;
+                next_ram_w_addr = '0;
+                next_in_counter = '0;
+                next_per_pool_counter = '0;
+                next_new_data_idx = '0;
+                // hand control to output mode if we are done with input
+                if(next_row_counter == POOL_SIZE) begin
+                    next_in_or_out = 1'b1;
+                    next_row_counter = '0;
+                end
+            end
+            // when processing not useful pixels
+            else begin
+                // if per_pool_counter_q not yet reset
+                if(per_pool_counter_q == POOL_SIZE + 2) begin
+                    // ram_w_data would have the correct values to be written in the ram now
+                    // increment ram_w_addr and have write_en high
+                    write_en = 1'b1;
+                    next_ram_w_addr = next_ram_w_addr + 1;
+                    // reset counters for next pool
+                    next_per_pool_counter = '0;
+                    next_new_data_idx = '0;
+                end
+                if(i_valid) begin
+                    next_in_counter = next_in_counter + 1;
+                end
+                // keep collecting useless data if there would be more
+                if(next_in_counter != WIDTH) begin
+                    next_i_ready = 1;
+                end
+            end
+        end
+        // if we are in output mode
+        else begin
+            // ram_r_addr would be 0 last cycle already so r_data are of valid values
+            o_data = ram_r_data;
+            o_valid = 1;
+            // if data will be consumed increment ram_r_addr so new value is shown next cycle
+            if(o_ready) begin
+                ram_r_addr = ram_r_addr + 1;
+            end
+            // if we are already showing the last data and the next data we are reading
+            // is from address out of bounds, then we have finished outputing,
+            // reset address and switch back to input mode
+            if(ram_r_addr == O_WIDTH) begin
+                ram_r_addr = '0;
+                next_in_or_out = 1'b0;
+            end
+        end
     end
 
-    // control signals
+    // keep this logic simple, control by using write_en
+    always_comb begin
+        ram_w_data = prev_max_q;
+        for(int i = 0; i < POOL_SIZE; i++) begin
+            if(new_data_q[i] > ram_w_data) ram_w_data = new_data_q[i];
+        end
+    end
+
+    // to log data into new_data_q
+    always_ff@(posedge clk) begin
+        if(new_data_idx_q < POOL_SIZE)
+            new_data_q[new_data_idx_q] <= i_data;
+    end
+
+    // ff
     always_ff@(posedge clk) begin
         if(reset) begin
-            o_data_q <= '0;
-            o_last_q <= 1'b0;
-            consumed <= 1'b1;
+            in_or_out_q <= 0;
+            i_ready_q <= 0;
+            ram_w_addr_q <= '0;
+            prev_ram_r_addr_q <= '0;
+            in_counter_q <= '0;
+            per_pool_counter_q <= '0;
+            row_counter_q <= '0;
+            prev_max_q <= '0;
+            new_data_idx_q <= '0;
         end
-        // if value is consumed and we got new stuff waiting for us
-        else if(consumed && buffer_valid) begin
-            o_data_q <= next_o_data;
-            o_last_q <= buffer_last;
-            consumed <= 1'b0;
-        end
-        // if value is going to be consumed and we got new stuff waiting for us
-        else if(~consumed && o_ready && buffer_valid) begin
-            o_data_q <= next_o_data;
-            o_last_q <= buffer_last;
-            consumed <= 1'b0;
-        end
-        // if value is going to be consumed and we dont have new stuff waiting for us
-        else if(~consumed && o_ready && ~buffer_valid) begin
-            consumed <= 1'b1;
+        else begin
+            in_or_out_q <= next_in_or_out;
+            i_ready_q <= next_i_ready;
+            ram_w_addr_q <= next_ram_w_addr;
+            prev_ram_r_addr_q <= ram_r_addr;
+            in_counter_q <= next_in_counter;
+            per_pool_counter_q <= next_per_pool_counter;
+            row_counter_q <= next_row_counter;
+            prev_max_q <= next_prev_max;
+            new_data_idx_q <= next_new_data_idx;
         end
     end
 
-    shift_buffer_array_pool #(
-        .WIDTH(WIDTH), 
-        .POOL_SIZE(POOL_SIZE),
-        .VALUE_BITS(VALUE_BITS)
-    ) buffer(
-        .clk(clk), 
-        .reset(reset),
-        // input interface 
-        .i_data(i_data),
-        .i_valid(i_valid), 
-        .i_ready(i_ready), 
-        .i_last(i_last),
-        
-        // output interface
-        .o_taps(buffer_taps), 
-        .o_valid(buffer_valid), 
-        .o_ready(buffer_ready),
-        .o_last(buffer_last)
-    );
+    always_ff@(posedge clk) begin
+        if(reset)
+            got_last_q <= 0;
+        else if(i_last)
+            got_last_q <= 1;
+    end
 
+
+    dual_port_ram #(
+        .VALUE_BITS(VALUE_BITS), 
+        .WIDTH(O_WIDTH)
+    ) line_ram(
+        .clk(clk), 
+        .w_data(ram_w_data), 
+        .w_addr(ram_w_addr_q),
+        .w_valid(write_en),
+        .r_addr(ram_r_addr), 
+        .r_data(ram_r_data)
+    );
 
 endmodule 
 
+/*
 module shift_buffer_array_pool #(
     parameter WIDTH, 
     parameter POOL_SIZE,
@@ -268,3 +423,4 @@ module shift_buffer_array_pool #(
     end
     
 endmodule 
+*/
