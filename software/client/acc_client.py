ac215692e0fdf51a0cc5821ed384ac43be4b437b
Trevor Galivan <trevorgalivan@gmail.com>
2023-11-21 18:59:52 -0500

    Updated acc_client

--- software/client/acc_client.py
+++ software/client/acc_client.py
@@ -4,22 +4,29 @@ from PIL import Image
 from time import sleep
 import subprocess
 
-os.system("rm -f ./test_files/*.result")
+#os.system("rm -f ./test_files/*.result")
+#
+subprocess.call("find ./test_files -maxdepth 1 -name \"*.result\" -print0 | xargs -0 rm", shell=True, executable="/bin/bash")
 
 ilist = os.listdir("./test_files")
 for f in ilist:
     if f[-4:] == ".png":
         im1 = Image.open("./test_files/" + f)
         im1.save("./test_files/" + f[0:-4] + ".jpg")
-os.system("rm -f ./test_files/*.png")
+#os.system("rm -f ./test_files/*.png")
+subprocess.call("rm -f ./test_files/*.png", shell=True, executable="/bin/bash")
 
 ilist = os.listdir("./test_files")
 
-totargs = ["./a.out"]
-for f in ilist:
-    totargs.append("./test_files/" + f)
+#totargs = ["./a.out"]
+#for f in ilist:
+#    totargs.append("test_files/" + f)
 
-subprocess.run(totargs)
+cmd = "./a.out test_files/*jpg"
+#for f in ilist:
+#    cmd += " test_files/" + f
+
+subprocess.call(cmd, shell=True, executable="/bin/bash")
 
 guesslist = [];
 
@@ -27,7 +34,7 @@ for f in ilist:
     with open("./test_files/"+f+".result", "rb") as file:
         cont = file.read();
         guesslist.append(struct.unpack("I" * (len(cont)//4), cont))
-        print(f, guesslist[-1])
+        #print(f, guesslist[-1])
 assert len(guesslist) == len(ilist)
 
 
